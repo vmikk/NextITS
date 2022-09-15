22695febc9a317f97783a12f931f15f9268a730a
Vladimir Mikryukov <v.mikryukov@gmail.com>
2022-09-15 13:53:04 +0300

    upd script for seq quality merging

--- bin/merge_sequnce_qualities.sh
+++ bin/merge_sequnce_qualities.sh
@@ -7,5 +7,6 @@ zcat "$1" \
   | awk \
     -F '\t' -v OFS='\t' \
     -v fnm="$2" \
-    '{ print $2 , $3 , $4 , fnm }' \
-  | sed 's/_hash_table.txt//'
+    'NR>1 { print fnm , $2 , $3 , $4 , $5 , $6 }' \
+  | sed 's/_hash_table.txt//' \
+  | sed '1i SampleID\tSeqID\tSeqLen\tPhredScore\tMaxEE\tMEEP'
