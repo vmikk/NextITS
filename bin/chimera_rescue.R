f4dd3b96205fa24391de9b22f77c7d36fb253f92
Vladimir Mikryukov <v.mikryukov@gmail.com>
2022-09-19 18:29:50 +0300

    add chimera rescue script

---
+++ bin/chimera_rescue.R
@@ -0,0 +1,56 @@
+#!/usr/bin/Rscript
+
+## Script to rescue sequences that were annotated as chimeric, 
+## but have high occurrence within sequenceing run (occurrence > 2) 
+
+# Input is given as positional arguments:
+#   1. List of all chimeric sequences (`All_chimeras.txt.gz`)
+#   2. Min sequence occurrence to be preserved (e.g., 2)
+#   3. Output file name (`Rescued_Chimeric_sequences.fa.gz`)
+
+suppressMessages(library(data.table)); setDTthreads(threads = 1)
+suppressMessages(library(Biostrings))
+
+args <- commandArgs(trailingOnly = TRUE)
+
+MINOCC <- as.numeric( args[2] )
+
+## Load sequences
+cat("..Loading chimeric sequences\n")
+CH <- fread(file = args[1],
+  sep = "\t", header = F,
+  col.names = c("SeqID", "Seq"))
+
+cat("..Total number of chimeric records: ", nrow(CH), "\n")
+
+if(nrow(CH) > 0){
+
+  ## Extract sample name and sequencing run ID
+  CH[, SampleID := tstrsplit(x = SeqID, split = ";", keep = 2) ]
+  CH[, SampleID := gsub(pattern = "sample=", replacement = "", x = SampleID) ]
+
+  ## Estimate sequence frequency
+  cat("..Estimating chimera occurrence\n")
+  CF <- CH[, .(Occurrence = .N), by = "Seq"]
+
+  ## Exclude sequences with low occurrence (most probably chimeric)
+  ## Sequences with higher occurrence should be "real" sequences
+  CF <- CF[ Occurrence > MINOCC ]
+
+  cat("..Total number of unique chimeric sequences: ", length(unique(CH$Seq)), ".\n")
+
+  ## Export sequences
+  if(nrow(CF) > 0){
+    cat("..There are", nrow(CF), "unique sequence to rescue.\n")
+    NCH <- CH[ Seq %in% CF$Seq ]
+    SQS <- DNAStringSet(x = NCH$Seq)
+    names(SQS) <- NCH$SeqID
+
+    cat("..Exporting rescued sequences\n")
+    writeXStringSet(x = SQS, filepath = args[3],
+      compress=TRUE, format="fasta", width=9999)
+  } else {
+    cat("..No sequences were rescued.\n")
+  }
+
+}
