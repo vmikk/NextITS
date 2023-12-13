044eda320df003e4fffdd9edf821ea54eea36454
Vladimir Mikryukov <v.mikryukov@gmail.com>
2023-12-13 14:36:03 +0200

    upd dada script

--- bin/dada2_no_quals.R
+++ bin/dada2_no_quals.R
@@ -1,3 +1,26 @@
 #!/usr/bin/env Rscript
 
 ## Perform sequence denoising with DADA2
+
+############################################## Load packages
+
+cat("Loading R packages...\n")
+
+load_pckg <- function(pkg = "data.table"){
+  suppressPackageStartupMessages( library(package = pkg, character.only = TRUE) )
+  cat(paste(pkg, packageVersion(pkg), "\n"))
+}
+
+load_pckg("Biostrings")
+load_pckg("ShortRead")
+load_pckg("data.table")
+load_pckg("dada2")
+
+cat("\n")
+
+## Set CPU thread number
+cat("Setting number of CPU threads to: ", CPUTHREADS, "\n")
+setDTthreads(threads = CPUTHREADS)  # for data.table
+
+## Set seed
+set.seed(111)
