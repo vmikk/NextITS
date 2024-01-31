1c477852457a5cf0b0e0ef004ee26fc2d06ba356
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-01-31 14:39:21 +0200

    `homopolymer_collapse_len`

--- bin/homopolymer_collapse_len.sh
+++ bin/homopolymer_collapse_len.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
 
+
+awk -v H="$1" '\
+
+BEGIN {
+    if (H < 1) H = 1;
+}
+
+# If the line is a header, print it as is
+/^>/ {
+    print;
+    next;
+}
+
+# Process sequence lines
+{
+    sequence = $0;
+    collapsedSeq = "";
+    count = 1;
+
+    for (i = 2; i <= length(sequence); i++) {
+        if (substr(sequence, i, 1) == substr(sequence, i - 1, 1)) {
+            count++;
+        } else {
+            collapsedSeq = collapsedSeq substr(sequence, i - count, (count > H) ? H : count);
+            count = 1;
+        }
+    }
+
+    # Handle the last homopolymer stretch
+    collapsedSeq = collapsedSeq substr(sequence, length(sequence) - count + 1, (count > H) ? H : count);
+
+    print collapsedSeq;
+}
+' "$2"
+
