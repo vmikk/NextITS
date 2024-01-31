44925b85047599dd9da644d61e122acb8d50b603
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-01-31 14:37:40 +0200

    hc compression - add script description

--- bin/homopolymer_compression.sh
+++ bin/homopolymer_compression.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Script to collapse homopolymer stretches to a single letter
+
 bioawk \
   '{ gsub(/[A]+/,"A");gsub(/[C]+/,"C");gsub(/[T]+/,"T");gsub(/[G]+/,"G");gsub(/[N]+/,"N") }1' \
   "$1"
