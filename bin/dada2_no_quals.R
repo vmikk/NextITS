817fdb8b079fe3677d2b00ed3172be4f3b36e23f
Vladimir Mikryukov <vmikryukov@gmail.com>
2025-08-25 09:41:12 +0300

    upd `dada2_no_quals`

--- bin/dada2_no_quals.R
+++ bin/dada2_no_quals.R
@@ -101,3 +101,84 @@ setDTthreads(threads = CPUTHREADS)  # for data.table
 
 ## Set seed
 set.seed(111)
+
+## Set DADA options
+cat("Setting DADA2 options\n")
+setDadaOpt(
+  BAND_SIZE         = BAND_SIZE,           # dada2 default, 16
+  DETECT_SINGLETONS = DETECT_SINGLETONS,   # dada2 default, FALSE
+  OMEGA_A           = OMEGA_A,             # dada2 default, 1e-40
+  OMEGA_C           = OMEGA_C,             # dada2 default, 1e-40
+  OMEGA_P           = OMEGA_P,             # dada2 default, 1e-4
+  MAX_CONSIST       = MAX_CONSIST,         # dada2 default, 10
+  GAP_PENALTY       = GAP_PENALTY,         # dada2 default, -8
+  MATCH             = MATCH,               # dada2 default, 4
+  MISMATCH          = MISMATCH,            # dada2 default, -5
+  HOMOPOLYMER_GAP_PENALTY = HOMOPOLYMER_GAP_PENALTY  # PacBio CCS does not make homopolymer errors at a higher rate than normal indels
+  )
+
+## Get DADA options
+# getDadaOpt()
+
+############################################## Workflow
+
+
+## Load FASTQ file
+fq <- readFastq(dirPath = INPUT, qualityType = "FastqQuality")
+
+## Extract sequence headers
+sq <- as.data.table(fq@id)
+setnames(x = sq, new = "SeqName")
+sq[ , c("SeqID", "Abundance") := tstrsplit(x = SeqName, split = ";size=", keep = 1:2) ]
+sq[ , Abundance := as.numeric(Abundance) ]
+sq[ , Sequence := as.character(sread(fq))]
+
+## Extract sequence qualities
+seq_quals <- as(quality(fq), "matrix")
+# dada2:::qtables2(fq)
+
+## Summary stats
+num_seqs  <- nrow(sq)
+num_singl <- nrow(sq[ Abundance < 2 ])
+num_reads <- sum(sq$Abundance, na.rm = TRUE)
+perc_nonsingleton <- (num_seqs - num_singl) / num_seqs * 100
+
+
+## Manually create a derep-class object
+##   See also https://github.com/benjjneb/dada2/blob/004ce26909268e1318a2f68e0ea26807412c7a2d/R/sequenceIO.R#L240-L242
+#             https://github.com/benjjneb/dada2/blob/004ce26909268e1318a2f68e0ea26807412c7a2d/R/sequenceIO.R#L45
+
+## Prepare derep-class object
+uniques <- sq$Abundance
+names(uniques) <- as.character(sread(fq))   # names = full amplicon sequence
+rownames(seq_quals) <- names(uniques)
+
+derep <- list(
+  uniques = uniques,
+  quals   = seq_quals,
+  map     = NULL,
+  SeqID   = sq$SeqID         # add allso sequence IDs
+  )
+
+derep <- as(derep, "derep")
+
+
+## Estimate error rates for each type of transition while ignoring quality scores
+errors <- try(
+  learnErrors(
+    fls    = derep,
+    nbases = NBASES,
+    errorEstimationFunction = noqualErrfun,
+    qualityType = "FastqQuality",
+    verbose     = 1,
+    multithread = CPUTHREADS
+    )
+  )
+
+
+## Export results
+saveRDS(object = errors,
+  file = "DADA2_ErrorRates_noqualErrfun.RData",
+  compress = "xz")
+
+
